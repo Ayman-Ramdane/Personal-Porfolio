7fe1d9f9a48ce1bff03ca27e97bfcb6b1c58bcf9
Ayman-Ramdane <ayman.ramdane@epfedu.fr>
2022-04-24 11:13:54 +0200

    Complete header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me_transparent_background.png'
+import HeraderSocials from './HeadderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Ayman RAMDANE</h1>
+        <h5 className="text-light">Developer</h5>
+        <CTA/>
+        <HeraderSocials/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
